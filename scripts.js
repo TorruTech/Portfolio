3b05bf1cecb30f1722d38d0715a6298b8a6790af
TorruTech <javiertp97@gmail.com>
2024-08-04 12:50:12 +0200

    folder structure changed again

--- scripts.js
+++ scripts.js
@@ -158,5 +158,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Update nav background on scroll
     window.addEventListener('scroll', updateNavBackground);
+
+
+
+
+    // ANIMATIONS 
+
+    const questionItems = document.querySelectorAll(".questionItem");
+
+    questionItems.forEach(item => {
+        const chevronRight = item.querySelector('.chevronRight');
+        const answer = item.querySelector('.answers');
+        const question = item.querySelector('.question')
+
+        chevronRight.addEventListener("click", () => {
+            // Cerrar todos los demás items
+            questionItems.forEach(otherItem => {
+                if (otherItem !== item) {
+                    const otherChevron = otherItem.querySelector('.chevronRight');
+                    const otherAnswer = otherItem.querySelector('.answers');
+                    const otherQuestion = otherItem.querySelector('.question')
+                    otherAnswer.classList.remove("active");
+                    otherChevron.style.transform = "rotate(0deg)";
+                    otherQuestion.style.color = "#fff"
+                }
+            });
+
+            // Alternar el estado del item actual
+            answer.classList.toggle("active");
+            if (answer.classList.contains("active")) {
+                chevronRight.style.transform = "rotate(90deg)";
+                question.style.color = "#FFBD39"
+            } else {
+                chevronRight.style.transform = "rotate(0deg)";
+                   question.style.color = "#fff"
+            }
+        });
+    });
 });
 
